e566183a46e8317f847d977567a2cdba4b0276a6
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2023-11-09 14:33:33 -0600

    get working still working on post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,70 @@
 console.log( 'js' );
 
 function getKoalas(){
-  console.log( 'in getKoalas' );
+  console.log('in getKoalas' );
   // axios call to server to get koalas
-  
-} // end getKoalas
-
-function saveKoala(){
-  console.log( 'in saveKoala' );
-  // axios call to server to get koalas
- 
+    // get song data from the server
+    axios({
+      method: 'GET',
+      url: '/koalas'
+    }).then(function (response) {
+      console.log('Got koalas', response.data);
+      renderKoalas(response.data);
+    }).catch(function (error) {
+      console.log('error in koala get', error);
+    });
 }
 
+function renderKoalas(koalasArray) {
+  // Store selector in variable
+  const koalaTableBody = document.getElementById('viewKoalas');
+
+  // Empty previous data
+  koalaTableBody.innerHTML = '';
+  // Add all songs to table
+  console.log(koalasArray);
+  for (let koala of koalasArray) {
+    koalaTableBody.innerHTML += (`
+      <tr>
+        <td>${koala.name}</td>
+        <td>${koala.age}</td>
+        <td>${koala.gender}</td>
+        <td>${koala.ready_for_transfer}</td>
+        <td>${koala.notes}</td>
+      </tr>`
+    );
+  }
+}
+// end getKoalas
+
+function addKoala(event) {
+  event.preventDefault();
+  // Get info to send to the server
+  koalaname = document.getElementById('nameIn').value; 
+  koalaage = document.getElementById('ageIn').value;
+  koalagender= document.getElementById('genderIn').value;
+  koalaready_for_transfer = document.getElementById('readyForTransferIn').value;
+  koalanotes = document.getElementById('notesIn').value;
+  const newKoala = {
+    name: koalaname, 
+    age: koalaage, 
+    gender: koalagender,
+    ready_for_transfer: koalaready_for_transfer,
+    notes: koalanotes}
+
+  console.log('Adding koala', newKoala);
+
+  // Send the new artist to the server as data
+  axios({
+    method: 'POST',
+    url: '/koalas',
+    data: newKoala
+  }).then(function(response) {
+    console.log(response.data);
+    getKoalas();
+  }).catch(function(error) {
+    console.log('error in Koala post', error); 
+    alert('Error adding koala. Please try again later.')       
+  });
+}
 getKoalas();
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,14 +1,57 @@
 const express = require('express');
 const koalaRouter = express.Router();
-
 // DB CONNECTION
-
+const pg = require('pg')
+const pool = new pg.Pool({
+    hostname: 'localhost',
+    port: 5432,
+    database: 'koala_Holla'
+  })
 
 // GET
-
+koalaRouter.get('/', (req, res) => {
+    const sqlQueryText = `
+      SELECT * FROM "koalas"
+        ORDER BY "name";
+    `
+    pool.query(sqlQueryText)
+      .then((dbResult) => {
+        console.log('dbResult.rows:', dbResult.rows)
+        res.send(dbResult.rows)
+      })
+      .catch((dbError) => {
+        res.sendStatus(500)
+      })
+  
+  });
 
 // POST
+koalaRouter.post('/koalas', (req, res) => {
+  console.log('POST /koalas got a request, here is req.body:')
+  console.log(req.body)
+  const sqlQueryText = `
+  INSERT INTO "koalas"
+  ("name", "age", "gender", "ready_for_transfer", "notes")
+  VALUES
+    ($1, $2, $3, $4, $5);
+  `
+  const sqlValues = [
+    req.body.name,
+    req.body.age,
+    req.body.gender,
+    req.body.ready_to_transfer,
+    req.body.note,
 
+
+  ]
+  pool.query(sqlQueryText, sqlValues)
+    .then((dbResult) => {
+      res.sendStatus(201)
+    })
+    .catch((dbError) => {
+      console.log('POST /koalas SQL query failed:', dbError)
+      res.sendStatus(500)
+    })})
 
 // PUT
 
